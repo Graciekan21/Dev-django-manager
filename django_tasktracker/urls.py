8c637430903440c7cc043a5b90276cee5f0d0a09
Graciekan21 Nemusesu <graciekan21@gmail.com>
2024-03-06 20:31:01 +0000

    Import the views function from views.py, Add your URL paths

--- django_tasktracker/urls.py
+++ django_tasktracker/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tasktracker.views import say_hello
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', say_hello, name = 'hello')
 ]
