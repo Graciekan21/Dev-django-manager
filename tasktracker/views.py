b28b6bb9559443712fba785560799b53d71d3d32
Graciekan21 Nemusesu <graciekan21@gmail.com>
2024-03-06 22:22:34 +0000

    Create new folders, file and create boilerplate

--- tasktracker/views.py
+++ tasktracker/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, 
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello!")
+def get_todo_list(request):
+    return render(request, 'tasktracker/tasktracker_list.html')
+
